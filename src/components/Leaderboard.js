41341d0f943da1162328fd8ba78cb68f8b0a3989
Francesco Giorgio <francesco@giorgio.dev>
2020-04-19 17:55:31 +0200

    feature: added leaderboard
    Refactored Leaderboard and UserScore components.
    Customized css styles.

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,26 +1,43 @@
 import React, {Component} from 'react'
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
+import UserScore from "./UserScore";
+import {Container, Row, Col} from "react-bootstrap";
 
 class Leaderboard extends Component {
-    render(){
+    render() {
 
-        const {authedUser} = this.props;
+        const {authedUser, users} = this.props;
+        let count = 1;
 
         // Redirect user if not authenticated
         if (authedUser === null) {
             return <Redirect to={{
                 pathname: '/login',
                 requestedURL: '/leaderboard',
-            }} />
+            }}/>
         }
 
         return (
-            <div>Leaderboard</div>
+            <Container fluid>
+                <Row>
+                    <Col md="8" lg="6" className="mx-auto my-5">
+                        {Object.keys(users)
+                            .sort((a, b) => (
+                                (Object.keys(users[b].answers).length + users[b].questions.length) - (Object.keys(users[a].answers).length + users[a].questions.length)
+                            ))
+                            .map((uid) => (
+                                <UserScore key={uid} pos={count++} user={users[uid]}/>
+                            ))
+                        }
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
 
 export default connect((state) => ({
     authedUser: state.authedUser,
+    users: state.users,
 }))(Leaderboard)
--- src/components/UserScore.js
+++ src/components/UserScore.js
@@ -1,9 +1,46 @@
 import React, {Component} from 'react'
+import {Card, Col, Image, Row} from "react-bootstrap";
 
 class UserScore extends Component {
-    render(){
+    render() {
+
+        const {user, pos} = this.props;
+
+        const answered = Object.keys(user.answers).length;
+        const created = user.questions.length;
+
         return (
-            <div>UserScore</div>
+            <Card className="my-3 leaderboard-card">
+                <Card.Header className="h5">
+                    #{pos}
+                </Card.Header>
+                <Card.Body>
+                    <Row>
+                        <Col xs="12" sm="auto">
+                            <Image
+                                className="leaderboard-avatar"
+                                src={user.avatarURL}
+                                roundedCircle
+                            />
+                        </Col>
+                        <Col>
+                            <h2 className="h5 mb-3">{user.name}</h2>
+                            <span className="d-block">Answered: {answered}</span>
+                            <span className="d-block">Created: {created}</span>
+                        </Col>
+                        <Col xs="12" sm="auto">
+                            <Card className="text-center leaderboard-score">
+                                <Card.Header className="p-1">Score</Card.Header>
+                                <Card.Body className="p-1">
+                                    <Card.Text className="h1">
+                                        {answered + created}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
         )
     }
 }
